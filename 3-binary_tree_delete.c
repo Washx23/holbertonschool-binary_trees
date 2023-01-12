b4a454119e4b681b484c1df8f13308e51c206033
Washx23 <5731@holbertonstudents.com>
2023-01-12 06:01:46 -0800

    fix 3-binary_tree_delete

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -5,8 +5,8 @@
  */
 void binary_tree_delete(binary_tree_t *tree)
 {
-	if (!tree)
-		exit(0);
+	if (tree = NULL)
+		return;
 	binary_tree_delete(tree->left);
 	binary_tree_delete(tree->right);
 	free(tree);
