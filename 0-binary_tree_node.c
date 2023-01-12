d69187fb70bf062bbe5a87a48cdf0b373a87e29a
Washx23 <5731@holbertonstudents.com>
2023-01-11 18:06:50 -0800

    first commit of binary trees

---
+++ 0-binary_tree_node.c
@@ -0,0 +1,20 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_node - function that creates a binary tree node
+ * @value: is the value to put in the new node
+ * @parent:  is a pointer to the parent node of the node to create
+ * Return: newNode
+ **/
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+	binary_tree_t *newNode =  malloc(sizeof(binary_tree_t));
+
+	if (!value)
+	{
+		free(newNode);
+		return (NULL);
+	}
+	newNode->parent = parent;
+	newNode->n = value;
+	return (newNode);
+}
