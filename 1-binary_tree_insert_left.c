1b0d3755b5169a438f9da50d1e1ea12e5ffe4cba
Washx23 <5731@holbertonstudents.com>
2023-01-12 04:43:19 -0800

    1-binary_tree_insert_left.c

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -8,11 +8,11 @@
  */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
+	if (!parent)
+		return(NULL);
 	binary_tree_t *newNode =  malloc(sizeof(binary_tree_t));
 	if (!newNode)
-	{
 		return (NULL);
-	}
 	newNode->parent = parent;
 	newNode->n = value;
 	if (parent->left)
