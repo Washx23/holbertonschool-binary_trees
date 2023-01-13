019d69b4619d9b931f1bc5b1b1e81f9db341e1f4
root <brunocarnales@gmail.com>
2023-01-13 11:26:32 -0800

    15-binary_tree_is_full.c

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -6,13 +6,11 @@
  */
 int binary_tree_balance(const binary_tree_t *tree)
 {
-	int left = 0, right = 0;
+	int left, right;
 
 	if (tree == NULL)
 		return (0);
-	if (tree->left)
-		left = 1 + binary_tree_balance(tree->left);
-	if (tree->right)
-		right = 1 + binary_tree_balance(tree->right);
+	left = binary_tree_height(tree->left);
+	right = binary_tree_height(tree->right);
 	return (left - right);
 }
---
+++ 15-binary_tree_is_full.c
@@ -0,0 +1,16 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_is_full - checks if a binary tree is full
+ * @tree: pointer to the root node of the tree to check
+ * Return: 0 
+ */
+int binary_tree_is_full(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return (0);
+	if (tree->left == NULL && tree->right == NULL)
+		return (1);
+	if (tree->left && tree->right)
+		return (binary_tree_is_full(tree->left) && binary_tree_is_full(tree->right));
+	return (0);
+}
